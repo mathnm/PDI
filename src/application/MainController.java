a514aa79524e88a6674a23967b97bd501e4cbd77
mathnm <matheusnuma@gmail.com>
2020-03-10 21:10:28 -0300

    upload de imagem e captura de rgb com mouse

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,5 +1,94 @@
 package application;
 
+import java.io.File;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+
 public class MainController {
+	
+	@FXML ImageView imageView1;
+	@FXML ImageView imageView2;
+	@FXML ImageView imageView3;
+	
+	@FXML Label lblR;
+	@FXML Label lblG;
+	@FXML Label lblB;
+	
+	private Image img1;
+	private Image img2;
+	private Image img3;
+	
+	@FXML
+	public void abreImagem1() {
+		abreImagem(imageView1, img1);
+	}
+	
+	@FXML
+	public void abreImagem2() {
+		abreImagem(imageView2, img2);
+	}
+	
+	private void atualizaImagem3() {
+		imageView3.setImage(img3);
+		imageView3.setFitHeight(img3.getHeight());
+		imageView3.setFitWidth(img3.getWidth());
+	}
+	
+	private void abreImagem(ImageView imageView, Image image) {
+		
+		File f = selecionaImagem();
+		if(f != null) {
+			image = new Image(f.toURI().toString());
+			imageView.setImage(image);
+			imageView.setFitHeight(image.getHeight());
+			imageView.setFitWidth(image.getWidth());
+		}
+		
+	}
+	
+	private File selecionaImagem() {
+		   FileChooser fileChooser = new FileChooser();
+		   fileChooser.getExtensionFilters().add(new 
+				   FileChooser.ExtensionFilter(
+						   "Imagens", "*.jpg", "*.JPG", 
+						   "*.png", "*.PNG", "*.gif", "*.GIF", 
+						   "*.bmp", "*.BMP")); 	
+		   fileChooser.setInitialDirectory(new File("C:\\Users\\mathe\\Pictures\\imgPDI"));
+		   File imgSelec = fileChooser.showOpenDialog(null);
+		   try {
+			   if (imgSelec != null) {
+			    return imgSelec;
+			   }
+		   } catch (Exception e) {
+			e.printStackTrace();
+		   }
+		   return null;
+	}
+	
+	
+	private void verificaCor(Image img, int x, int y){
+		  try {
+				Color cor = img.getPixelReader().getColor(x-1, y-1);
+				lblR.setText("R: "+(int) (cor.getRed()*255));
+				lblG.setText("G: "+(int) (cor.getGreen()*255));
+				lblB.setText("B: "+(int) (cor.getBlue()*255));
+			} catch (Exception e) {
+				//e.printStackTrace();
+			}
+	  }
+	
+	 @FXML
+	 public void rasterImg(MouseEvent evt){
+		 ImageView iw = (ImageView)evt.getTarget();
+		 if(iw.getImage()!=null)
+			 verificaCor(iw.getImage(), (int)evt.getX(), (int)evt.getY());
+	  }
+	
 	
 }
