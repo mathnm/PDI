329f4dc990bbd12577472c3fec8ad41f174b4b90
mathnm <matheusnuma@gmail.com>
2020-03-19 14:00:13 -0300

    implementado limiarização

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -4,6 +4,8 @@ import java.io.File;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -24,6 +26,12 @@ public class MainController {
 	private Image img1;
 	private Image img2;
 	private Image img3;
+	
+	@FXML TextField pcr;
+	@FXML TextField pcg;
+	@FXML TextField pcb;
+	
+	@FXML Slider limiar;
 	
 	@FXML
 	public void abreImagem1() {
@@ -98,6 +106,20 @@ public class MainController {
 		 img3 = Pdi.cinzaMediaAritmetica(img1, 0, 0, 0);
 		 atualizaImagem3();
 	 }
-	
+	 
+	 @FXML
+	 public void cinzaPonderada() {
+		 int r = Integer.parseInt(pcr.getText());
+		 int g = Integer.parseInt(pcg.getText());
+		 int b = Integer.parseInt(pcb.getText());
+		 img3 = Pdi.cinzaMediaAritmetica(img1, r, g, b);
+		 atualizaImagem3();
+	 }
+	 
+	 @FXML	 	 
+	 public void limiariza() {
+		 img3 = Pdi.limiarizacao(img1, limiar.getValue()/255);
+		 atualizaImagem3();
+	 }
 	
 }
--- src/view/Pdi.java
+++ src/view/Pdi.java
@@ -17,16 +17,50 @@ public class Pdi {
 			WritableImage wi = new WritableImage(w,h);
 			PixelWriter pw = wi.getPixelWriter();
 			
+			double media;
+			
 			for(int i=0; i<w; i++) {
 				for(int j=0; j<h; j++) {
 					Color corA = pr.getColor(i,j);
-					double media = (corA.getRed()+corA.getGreen()+corA.getBlue())/3;
-					if(pcr != 0 || pcg != 0 || pcb != 0)
+					if(pcr != 0 || pcg != 0 || pcb != 0) {
 						media = (corA.getRed()*pcr + corA.getGreen()*pcg +corA.getBlue()*pcb)/100;
+					} else { 
+						media = (corA.getRed()+corA.getGreen()+corA.getBlue())/3;
+					}
 					Color corN = new Color(media, media, media, corA.getOpacity());
 					pw.setColor(i, j, corN);
 				}
 			}
+			return wi;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public static Image limiarizacao(Image imagem, double limiar) {
+		try {
+			int w = (int)imagem.getWidth();
+			int h = (int)imagem.getHeight();
+			
+			PixelReader pr = imagem.getPixelReader();
+			WritableImage wi = new WritableImage(w, h);
+			PixelWriter pw = wi.getPixelWriter();
+			
+			for (int i = 0; i < w; i++) {
+				for (int j = 0; j < h; j++) {
+					Color corA = pr.getColor(i, j);
+					Color corN;
+					
+					if(corA.getRed() >= limiar) {
+						corN = new Color(1,1,1,corA.getOpacity());
+					} else {
+						corN = new Color(0,0,0,corA.getOpacity());
+					}
+					pw.setColor(i, j, corN);
+				}
+			}
+			
 			return wi;
 		} catch (Exception e) {
 			e.printStackTrace();
